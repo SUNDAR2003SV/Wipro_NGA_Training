622749835eac41e596b166389ffb52fa4d24a789
SUNDAR2003SV <120670633+SUNDAR2003SV@users.noreply.github.com>
2026-04-16 23:54:54 +0530

    Add files via upload
    Day 1 , Program - 2 (C# Program to add two Numbers)

--- Day_1/Program.cs
+++ Day_1/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Enter the first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter the second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int sum = num1 + num2;
+Console.WriteLine("The Sum is: " + sum);
+
+
