b31613a14e22c83dea30b124bf6a2f20d5f5a6f2
SUNDAR2003SV <120670633+SUNDAR2003SV@users.noreply.github.com>
2026-04-17 00:01:26 +0530

    Add files via upload
    C# Program to get book details from the user and display

--- Day_2/Book_Details_Input_and_output/Program.cs
+++ Day_2/Book_Details_Input_and_output/Program.cs
@@ -1,1 +1,43 @@
+﻿using System;
 
+class Book
+{
+  
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public int Year { get; set; }
+
+  
+    public Book(string title, string author, int year)
+    {
+        Title = title;
+        Author = author;
+        Year = year;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+      
+        Console.WriteLine("Enter book's title:");
+        string title = Console.ReadLine();
+
+       
+        Console.WriteLine("Enter book's author:");
+        string author = Console.ReadLine();
+
+    
+        Console.WriteLine("Enter book's year:");
+        int year = Convert.ToInt32(Console.ReadLine());
+
+        Book book = new Book(title, author, year);
+
+
+        Console.WriteLine("\nBook Details:");
+        Console.WriteLine("Title: " + book.Title);
+        Console.WriteLine("Author: " + book.Author);
+        Console.WriteLine("Year: " + book.Year);
+    }
+}
