978c1b5a89331cdb6e03e773014267584a9fe60b
SUNDAR2003SV <120670633+SUNDAR2003SV@users.noreply.github.com>
2026-04-18 15:08:32 +0530

    Add files via upload
    Program that uses OOP concepts to create, display, and update student details using a class, constructor, and methods

--- Day_3/Student_Class_Program/Program.cs
+++ Day_3/Student_Class_Program/Program.cs
@@ -1,1 +1,53 @@
+﻿using System;
 
+class Student
+{
+    
+    private string name;
+    private int id;
+
+    public Student(string name, int id)
+    {
+        this.name = name;
+        this.id = id;
+    }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine("Student Name: " + name);
+        Console.WriteLine("Student ID: " + id);
+    }
+
+    public void UpdateName(string newName)
+    {
+        name = newName;
+    }
+
+    public void UpdateId(int newId)
+    {
+        id = newId;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Student student1 = new Student("John Doe", 101);
+        Student student2 = new Student("Tywin", 102);
+
+        Console.WriteLine("Original Student Details:");
+        student1.DisplayInfo();
+        student2.DisplayInfo();
+
+        student1.UpdateName("John Snow");
+        student1.UpdateId(201);
+
+        student2.UpdateName("Tyrion Lannister");
+        student2.UpdateId(202);
+
+        Console.WriteLine("Updated Student Details:");
+        student1.DisplayInfo();
+        student2.DisplayInfo();
+    }
+}
