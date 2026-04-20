1c1a09b4b4b4c2a2a5e5274ee9025a0f1dc486a1
SUNDAR2003SV <120670633+SUNDAR2003SV@users.noreply.github.com>
2026-04-20 19:54:16 +0530

    Add files via upload

--- Day_5/Exception_Handling_Example/Program.cs
+++ Day_5/Exception_Handling_Example/Program.cs
@@ -1,1 +1,32 @@
+﻿using System;
 
+// Custom Exception
+public class InvalidAgeException : Exception
+{
+    public InvalidAgeException(string message) : base(message)
+    {
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Console.WriteLine("Enter age:");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            if (age < 18)
+            {
+                throw new InvalidAgeException("Age must be 18 or above");
+            }
+
+            Console.WriteLine("Valid age");
+        }
+        catch (InvalidAgeException ex)
+        {
+            Console.WriteLine("Custom Exception: " + ex.Message);
+        }
+    }
+}
