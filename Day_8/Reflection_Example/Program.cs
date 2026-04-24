ab023c6c893d7f8fa8b6938b6b359c4382c44e2b
SUNDAR2003SV <120670633+SUNDAR2003SV@users.noreply.github.com>
2026-04-24 09:03:59 +0530

    Add files via upload

--- Day_8/Reflection_Example/Program.cs
+++ Day_8/Reflection_Example/Program.cs
@@ -1,1 +1,20 @@
+﻿using System;
 
+namespace reflectionExample
+
+
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Name: {Name}, Age: {Age}");
+        }
+        public void Exam()
+        {
+            Console.WriteLine("Student is taking an exam.");
+        }
+    }
+}
