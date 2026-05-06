b32e5e3ddad1893075f49402f85b721c45f26c43
SUNDAR2003SV <120670633+SUNDAR2003SV@users.noreply.github.com>
2026-05-06 16:07:23 +0530

    Add files via upload

--- Day_18/StudentFormApp/Pages/Index.cshtml.cs
+++ Day_18/StudentFormApp/Pages/Index.cshtml.cs
@@ -1,1 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
+namespace StudentApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        // Two-way binding property
+        [BindProperty]
+        public string StudentName { get; set; }
+
+        public string Message { get; set; }
+
+        // Handles GET request
+        public void OnGet()
+        {
+        }
+
+        // Handles POST request
+        public void OnPost()
+        {
+            if (!string.IsNullOrEmpty(StudentName))
+            {
+                Message = $"Welcome, {StudentName}! Your registration is successful.";
+            }
+            else
+            {
+                Message = "Please enter your name.";
+            }
+        }
+    }
+}
