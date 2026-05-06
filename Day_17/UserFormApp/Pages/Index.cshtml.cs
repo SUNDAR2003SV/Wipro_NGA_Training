c6b1a106a92a1447c3360f119686d408a2712888
SUNDAR2003SV <120670633+SUNDAR2003SV@users.noreply.github.com>
2026-05-06 10:33:04 +0530

    Add files via upload

--- Day_17/UserFormApp/Pages/Index.cshtml.cs
+++ Day_17/UserFormApp/Pages/Index.cshtml.cs
@@ -1,1 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
+namespace UserFormApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        [BindProperty]
+        public string Username { get; set; }
+
+        [BindProperty]
+        public string Email { get; set; }
+
+        public string Message { get; set; }
+
+        // 🔹 GET - page load
+        public void OnGet()
+        {
+            Message = "Enter your details";
+        }
+
+        // 🔹 POST - form submit
+        public void OnPost()
+        {
+            Message = $"Received: {Username} - {Email}";
+        }
+    }
+}
