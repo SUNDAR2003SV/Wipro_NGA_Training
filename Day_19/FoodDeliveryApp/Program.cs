eaa2cada95438efd4165f229073710899b59bd98
SUNDAR2003SV <120670633+SUNDAR2003SV@users.noreply.github.com>
2026-05-08 11:28:10 +0530

    Add files via upload

--- Day_19/FoodDeliveryApp/Program.cs
+++ Day_19/FoodDeliveryApp/Program.cs
@@ -1,1 +1,23 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+var app = builder.Build();
 
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "foodOrder",
+    pattern: "order-food",
+    defaults: new { controller = "Restaurant", action = "OrderPage" });
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Restaurant}/{action=Menu}/{id?}");
+
+app.MapControllers();
+app.Run();
