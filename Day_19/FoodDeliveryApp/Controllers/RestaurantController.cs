f1c97d366be0f79c823b7a992932c79f803eb722
SUNDAR2003SV <120670633+SUNDAR2003SV@users.noreply.github.com>
2026-05-08 11:22:06 +0530

    Add files via upload

--- Day_19/FoodDeliveryApp/Controllers/RestaurantController.cs
+++ Day_19/FoodDeliveryApp/Controllers/RestaurantController.cs
@@ -1,1 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
 
+namespace FoodDeliveryApp.Controllers
+{
+    public class RestaurantController : Controller
+    {
+        // Works with conventional: /Restaurant/Menu  OR attribute: /restaurant/menu
+        [Route("restaurant/menu")]
+        public IActionResult Menu()
+        {
+            return Content("Restaurant Menu Page");
+        }
+
+        [Route("restaurant/details/{id:int}")] // US-104: constraint
+        public IActionResult Details(int id)
+        {
+            return Content("Restaurant ID: " + id);
+        }
+
+        public IActionResult OrderPage() // For custom route /order-food
+        {
+            return Content("Food Ordering Page");
+        }
+    }
+}
