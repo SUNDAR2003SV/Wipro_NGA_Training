14f9a959861a688ff725a0622119395c5116f31e
SUNDAR2003SV <120670633+SUNDAR2003SV@users.noreply.github.com>
2026-05-11 21:03:50 +0530

    Add files via upload

---
+++ Day_20/RoutingDemoMVC/Controllers/RestauranctController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RoutingDemoMvc.Controllers
+{
+    public class RestaurantController : Controller
+    {
+        // DEFAULT ROUTE → /
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // CUSTOM ROUTE → /order-food
+        public IActionResult Menu()
+        {
+            return View();
+        }
+
+        // ATTRIBUTE ROUTING → /details/5
+        [Route("details/{id}")]
+        public IActionResult Details(int id)
+        {
+            ViewBag.Id = id;
+            return View();
+        }
+    }
+}
