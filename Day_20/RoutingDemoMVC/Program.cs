4fd2f5460794aff58b0e2f9e83fc41fc8cc5c83f
SUNDAR2003SV <120670633+SUNDAR2003SV@users.noreply.github.com>
2026-05-11 21:02:34 +0530

    Add files via upload

--- Day_20/RoutingDemoMVC/Program.cs
+++ Day_20/RoutingDemoMVC/Program.cs
@@ -1,1 +1,24 @@
+var builder = WebApplication.CreateBuilder(args);
 
+// Add MVC services
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+app.UseRouting();
+
+// DEFAULT ROUTING
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Restaurant}/{action=Index}/{id?}"
+);
+
+// CUSTOM ROUTE
+app.MapControllerRoute(
+    name: "foodRoute",
+    pattern: "order-food",
+    defaults: new { controller = "Restaurant", action = "Menu" }
+);
+
+app.Run();
