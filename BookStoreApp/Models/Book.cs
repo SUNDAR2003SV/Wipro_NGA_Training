06c6263806db5017da3f5c696fffac28a0236433
SUNDAR2003SV <120670633+SUNDAR2003SV@users.noreply.github.com>
2026-05-14 19:39:15 +0530

    Add files via upload

--- BookStoreApp/Models/Book.cs
+++ BookStoreApp/Models/Book.cs
@@ -1,1 +1,11 @@
-
+namespace BookStoreApp.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public decimal Price { get; set; }
+        public int PublishedYear { get; set; }
+    }
+}
