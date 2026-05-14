862a9abd921e0692948ef517dc2b238c58cee0fb
SUNDAR2003SV <120670633+SUNDAR2003SV@users.noreply.github.com>
2026-05-14 19:39:52 +0530

    Add files via upload

--- BookStoreApp/Program.cs
+++ BookStoreApp/Program.cs
@@ -1,1 +1,17 @@
+using BookStoreApp.Data;
 
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+
+// Register repository
+builder.Services.AddScoped<BookRepository>();
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+app.UseRouting();
+
+app.MapRazorPages();
+
+app.Run();
--- BookStoreApp/SQLQueryBookStoreApp.sql
+++ BookStoreApp/SQLQueryBookStoreApp.sql
@@ -1,1 +1,35 @@
+CREATE DATABASE BookStoreDB;
 
+USE BookStoreDB;
+
+CREATE TABLE Books (
+    BookId INT PRIMARY KEY IDENTITY(1,1),
+    Title NVARCHAR(100) NOT NULL,
+    Author NVARCHAR(100) NOT NULL,
+    Price DECIMAL(10,2) NOT NULL,
+    PublishedYear INT
+);
+
+GO
+
+CREATE PROCEDURE sp_AddBook
+    @Title NVARCHAR(100),
+    @Author NVARCHAR(100),
+    @Price DECIMAL(10,2),
+    @PublishedYear INT
+AS
+BEGIN
+    INSERT INTO Books (Title, Author, Price, PublishedYear)
+    VALUES (@Title, @Author, @Price, @PublishedYear)
+END
+GO
+
+SELECT DB_NAME();
+
+EXEC sp_AddBook 
+    @Title = 'Clean Code',
+    @Author = 'Robert C. Martin',
+    @Price = 499.00,
+    @PublishedYear = 2008;
+
+SELECT * FROM Books;
