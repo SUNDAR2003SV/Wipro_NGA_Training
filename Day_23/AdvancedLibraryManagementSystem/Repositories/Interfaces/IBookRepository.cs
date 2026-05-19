add4ecfd41f237f5ea265dd00d50373b1d71f4b2
SUNDAR2003SV <120670633+SUNDAR2003SV@users.noreply.github.com>
2026-05-19 22:49:49 +0530

    Add files via upload

--- Day_23/AdvancedLibraryManagementSystem/Repositories/Interfaces/IBookRepository.cs
+++ Day_23/AdvancedLibraryManagementSystem/Repositories/Interfaces/IBookRepository.cs
@@ -1,1 +1,9 @@
+using LibraryManagementSystem.Models;
 
+namespace LibraryManagementSystem.Repositories.Interfaces
+{
+    public interface IBookRepository : IGenericRepository<Book>
+    {
+        Task<IEnumerable<Book>> GetBooksWithDetails();
+    }
+}
