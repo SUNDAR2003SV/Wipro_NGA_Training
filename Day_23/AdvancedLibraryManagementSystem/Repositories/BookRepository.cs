236b0fa140014722961aaa061ea8ea159eaf4f68
SUNDAR2003SV <120670633+SUNDAR2003SV@users.noreply.github.com>
2026-05-19 22:49:09 +0530

    Add files via upload

--- Day_23/AdvancedLibraryManagementSystem/Repositories/BookRepository.cs
+++ Day_23/AdvancedLibraryManagementSystem/Repositories/BookRepository.cs
@@ -1,1 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using LibraryManagementSystem.Data;
+using LibraryManagementSystem.Models;
+using LibraryManagementSystem.Repositories.Interfaces;
 
+namespace LibraryManagementSystem.Repositories
+{
+    public class BookRepository : GenericRepository<Book>, IBookRepository
+    {
+        private readonly LibraryDbContext _context;
+
+        public BookRepository(LibraryDbContext context)
+            : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Book>> GetBooksWithDetails()
+        {
+            return await _context.Books
+                .Include(b => b.Author)
+                .Include(b => b.Genre)
+                .ToListAsync();
+        }
+    }
+}
