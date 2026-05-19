7d59b5e4df7bebd59e0be033cbb2dab586e9e941
SUNDAR2003SV <120670633+SUNDAR2003SV@users.noreply.github.com>
2026-05-19 22:05:51 +0530

    Add files via upload

--- LibrarySystem/Program.cs
+++ LibrarySystem/Program.cs
@@ -1,1 +1,16 @@
+using LibrarySystem.Data;
+using Microsoft.EntityFrameworkCore;
 
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<LibraryDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("LibraryConnection")));
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+app.MapControllers();
+
+app.Run();
