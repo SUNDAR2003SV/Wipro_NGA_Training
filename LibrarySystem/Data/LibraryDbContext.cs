edc667ac10d700c9989116e0478ac66db13e82cd
SUNDAR2003SV <120670633+SUNDAR2003SV@users.noreply.github.com>
2026-05-19 22:05:24 +0530

    Add files via upload

--- LibrarySystem/Data/LibraryDbContext.cs
+++ LibrarySystem/Data/LibraryDbContext.cs
@@ -1,1 +1,32 @@
+using LibrarySystem.Models;
+using Microsoft.EntityFrameworkCore;
 
+namespace LibrarySystem.Data
+{
+    public class LibraryDbContext : DbContext
+    {
+        public LibraryDbContext(DbContextOptions<LibraryDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+
+        public DbSet<Author> Authors { get; set; }
+
+        public DbSet<Genre> Genres { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>()
+                .HasOne(b => b.Author)
+                .WithMany(a => a.Books)
+                .HasForeignKey(b => b.AuthorID);
+
+            modelBuilder.Entity<Book>()
+                .HasMany(b => b.Genres)
+                .WithMany(g => g.Books)
+                .UsingEntity(j => j.ToTable("BookGenres"));
+        }
+    }
+}
