38548ed8439141c4bd004b8d5ba3c898e6b69145
SUNDAR2003SV <120670633+SUNDAR2003SV@users.noreply.github.com>
2026-05-19 22:48:09 +0530

    Add files via upload

--- Day_23/AdvancedLibraryManagementSystem/Data/LibraryDbContext.cs
+++ Day_23/AdvancedLibraryManagementSystem/Data/LibraryDbContext.cs
@@ -1,1 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using LibraryManagementSystem.Models;
 
+namespace LibraryManagementSystem.Data
+{
+    public class LibraryDbContext : DbContext
+    {
+        public LibraryDbContext(DbContextOptions<LibraryDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+
+        public DbSet<Author> Authors { get; set; }
+
+        public DbSet<Genre> Genres { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>()
+                .Property(b => b.Price)
+                .HasPrecision(18, 2);
+        }
+    }
+}
