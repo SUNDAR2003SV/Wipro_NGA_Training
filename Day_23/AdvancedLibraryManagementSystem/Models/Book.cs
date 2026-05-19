a476c2d31ad1a24a35bb8780235689a9908a39ce
SUNDAR2003SV <120670633+SUNDAR2003SV@users.noreply.github.com>
2026-05-19 22:48:42 +0530

    Add files via upload

--- Day_23/AdvancedLibraryManagementSystem/Models/Book.cs
+++ Day_23/AdvancedLibraryManagementSystem/Models/Book.cs
@@ -1,1 +1,24 @@
+using System.ComponentModel.DataAnnotations;
 
+namespace LibraryManagementSystem.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int PublishedYear { get; set; }
+
+        public int AuthorId { get; set; }
+
+        public int GenreId { get; set; }
+
+        public Author Author { get; set; }
+
+        public Genre Genre { get; set; }
+    }
+}
--- Day_23/AdvancedLibraryManagementSystem/Models/Genre.cs
+++ Day_23/AdvancedLibraryManagementSystem/Models/Genre.cs
@@ -1,1 +1,11 @@
+namespace LibraryManagementSystem.Models
+{
+    public class Genre
+    {
+        public int GenreId { get; set; }
 
+        public string GenreName { get; set; }
+
+        public ICollection<Book> Books { get; set; }
+    }
+}
