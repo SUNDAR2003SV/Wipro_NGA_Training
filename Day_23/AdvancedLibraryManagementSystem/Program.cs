04f62bf0f348e253ec76c1f28332978965132520
SUNDAR2003SV <120670633+SUNDAR2003SV@users.noreply.github.com>
2026-05-19 22:50:17 +0530

    Add files via upload

--- Day_23/AdvancedLibraryManagementSystem/Program.cs
+++ Day_23/AdvancedLibraryManagementSystem/Program.cs
@@ -1,1 +1,48 @@
+using LibraryManagementSystem.Data;
+using LibraryManagementSystem.Repositories;
+using LibraryManagementSystem.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
+var builder = WebApplication.CreateBuilder(args);
+
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+
+// Configure SQL Server Connection
+builder.Services.AddDbContext<LibraryDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+// Register Repositories
+builder.Services.AddScoped<IBookRepository, BookRepository>();
+
+
+var app = builder.Build();
+
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+
+// Default Route
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
