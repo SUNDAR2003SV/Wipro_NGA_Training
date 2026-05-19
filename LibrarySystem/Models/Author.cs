4505334d0f7f800a60e3e07eef2f7ad8e226417f
SUNDAR2003SV <120670633+SUNDAR2003SV@users.noreply.github.com>
2026-05-19 22:04:56 +0530

    Add files via upload

--- LibrarySystem/Models/Author.cs
+++ LibrarySystem/Models/Author.cs
@@ -1,1 +1,13 @@
+namespace LibrarySystem.Models
+{
+    public class Author
+    {
+        public int AuthorID { get; set; }
 
+        public string Name { get; set; }
+
+        public string Bio { get; set; }
+
+        public ICollection<Book>? Books { get; set; }
+    }
+}
--- LibrarySystem/Models/Genre.cs
+++ LibrarySystem/Models/Genre.cs
@@ -1,1 +1,11 @@
+namespace LibrarySystem.Models
+{
+    public class Genre
+    {
+        public int GenreID { get; set; }
 
+        public string GenreName { get; set; }
+
+        public ICollection<Book>? Books { get; set; }
+    }
+}
