1d3db90cfea8e082280c22777650999ffdb76a4d
SUNDAR2003SV <120670633+SUNDAR2003SV@users.noreply.github.com>
2026-05-20 11:58:55 +0530

    Add files via upload

--- SecureShoppingApp/Models/ApplicationUser.cs
+++ SecureShoppingApp/Models/ApplicationUser.cs
@@ -1,1 +1,9 @@
+using Microsoft.AspNetCore.Identity;
 
+namespace SecureShoppingApp.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string RoleName { get; set; }
+    }
+}
---
+++ SecureShoppingApp/Models/Product.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace SecureShoppingApp.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(1, 100000)]
+        public decimal Price { get; set; }
+
+        [StringLength(500)]
+        public string Description { get; set; }
+    }
+}
