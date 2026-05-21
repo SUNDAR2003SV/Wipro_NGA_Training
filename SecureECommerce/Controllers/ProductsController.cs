3037f7a6eca5907c2bc22dda8d554895067c179c
SUNDAR2003SV <120670633+SUNDAR2003SV@users.noreply.github.com>
2026-05-21 21:18:56 +0530

    Add files via upload

---
+++ SecureECommerce/Controllers/ProductsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SecureECommerce.Controllers
+{
+    [Authorize]  // Any logged-in user
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
