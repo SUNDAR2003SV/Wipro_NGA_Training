228b4d530aed21cc78e4b9e59f0f5886b6343354
SUNDAR2003SV <120670633+SUNDAR2003SV@users.noreply.github.com>
2026-05-21 21:34:53 +0530

    Add files via upload

--- Day_31/SmartInventory_API_RoutingDemo/Models/Product.cs
+++ Day_31/SmartInventory_API_RoutingDemo/Models/Product.cs
@@ -1,1 +1,13 @@
+﻿namespace SmartInventory_API_RoutingDemo.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
 
+        public required string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int Quantity { get; set; } = 0;
+    }
+}
