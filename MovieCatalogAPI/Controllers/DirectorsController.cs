364533394848fe45d2c627214201d7bc91120565
SUNDAR2003SV <120670633+SUNDAR2003SV@users.noreply.github.com>
2026-05-29 10:40:11 +0530

    Add files via upload

---
+++ MovieCatalogAPI/Controllers/DirectorsController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MovieCatalogAPI.Data;
+using MovieCatalogAPI.Models;
+
+namespace MovieCatalogAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DirectorsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DirectorsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/directors
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Director>>> GetDirectors()
+        {
+            return await _context.Directors.ToListAsync();
+        }
+
+        // GET: api/directors/1/movies
+        [HttpGet("{directorId}/movies")]
+        public async Task<ActionResult<IEnumerable<Movie>>> GetMoviesByDirector(int directorId)
+        {
+            var director = await _context.Directors.FindAsync(directorId);
+
+            if (director == null)
+            {
+                return NotFound("Director not found");
+            }
+
+            var movies = await _context.Movies
+                .Where(m => m.DirectorId == directorId)
+                .ToListAsync();
+
+            return movies;
+        }
+    }
+}
--- MovieCatalogAPI/Controllers/MoviesController.cs
+++ MovieCatalogAPI/Controllers/MoviesController.cs
@@ -1,1 +1,89 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MovieCatalogAPI.Data;
+using MovieCatalogAPI.Models;
 
+namespace MovieCatalogAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MoviesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/movies
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Movie>>> GetMovies()
+        {
+            return await _context.Movies
+                .Include(m => m.Director)
+                .ToListAsync();
+        }
+
+        // GET: api/movies/1
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Movie>> GetMovie(int id)
+        {
+            var movie = await _context.Movies
+                .Include(m => m.Director)
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return movie;
+        }
+
+        // POST: api/movies
+        [HttpPost]
+        public async Task<ActionResult<Movie>> CreateMovie(Movie movie)
+        {
+            _context.Movies.Add(movie);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetMovie),
+                new { id = movie.Id }, movie);
+        }
+
+        // PUT: api/movies/1
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateMovie(int id, Movie movie)
+        {
+            if (id != movie.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(movie).State = EntityState.Modified;
+
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // DELETE: api/movies/1
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteMovie(int id)
+        {
+            var movie = await _context.Movies.FindAsync(id);
+
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            _context.Movies.Remove(movie);
+
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
