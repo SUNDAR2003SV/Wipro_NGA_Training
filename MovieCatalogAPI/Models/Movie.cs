365c6a6c22a992299466af0be974970af57fd2ed
SUNDAR2003SV <120670633+SUNDAR2003SV@users.noreply.github.com>
2026-05-29 10:40:39 +0530

    Add files via upload

--- MovieCatalogAPI/Models/Movie.cs
+++ MovieCatalogAPI/Models/Movie.cs
@@ -1,1 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace MovieCatalogAPI.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public int ReleaseYear { get; set; }
+
+        // Foreign Key
+        public int DirectorId { get; set; }
+
+        [ForeignKey("DirectorId")]
+        public Director? Director { get; set; }
+    }
+}
