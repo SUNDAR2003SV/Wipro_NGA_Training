e699de7fc25b52eed9600b1cf1d67d10a6b85353
SUNDAR2003SV <120670633+SUNDAR2003SV@users.noreply.github.com>
2026-05-29 10:39:43 +0530

    Add files via upload

---
+++ MovieCatalogAPI/Program.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using MovieCatalogAPI.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
