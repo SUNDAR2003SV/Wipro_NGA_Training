2790dd709901da858219ea5752712087f82d5f1e
SUNDAR2003SV <120670633+SUNDAR2003SV@users.noreply.github.com>
2026-05-29 10:42:20 +0530

    Add files via upload

--- MovieCatalogAPI/Data/ApplicationDbContext.cs
+++ MovieCatalogAPI/Data/ApplicationDbContext.cs
@@ -1,1 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieCatalogAPI.Models;
 
+namespace MovieCatalogAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+
+        public DbSet<Director> Directors { get; set; }
+    }
+}
