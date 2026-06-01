4907372e6e769a1a125400e8c4e086c5a565ec02
SUNDAR2003SV <120670633+SUNDAR2003SV@users.noreply.github.com>
2026-06-01 10:36:46 +0530

    Add files via upload

--- BookStoreAPI/Data/AppDbContext.cs
+++ BookStoreAPI/Data/AppDbContext.cs
@@ -1,1 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BookStoreApi.Models;
 
+namespace BookStoreApi.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+
+        public DbSet<Author> Authors { get; set; }
+    }
+}
