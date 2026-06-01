08eb283dcbaddfba81b3e2ebcd17735f57712075
SUNDAR2003SV <120670633+SUNDAR2003SV@users.noreply.github.com>
2026-06-01 10:36:22 +0530

    Add files via upload

---
+++ BookStoreAPI/Controllers/AuthorsController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BookStoreApi.Data;
+using BookStoreApi.Models;
+
+namespace BookStoreApi.Controllers
+{
+    [Route("api/authors")]
+    [ApiController]
+    public class AuthorsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public AuthorsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/authors
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Author>>> GetAuthors()
+        {
+            return await _context.Authors.ToListAsync();
+        }
+
+        // GET: api/authors/1
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Author>> GetAuthor(int id)
+        {
+            var author = await _context.Authors.FindAsync(id);
+
+            if (author == null)
+            {
+                return NotFound();
+            }
+
+            return author;
+        }
+
+        // POST: api/authors
+        [HttpPost]
+        public async Task<ActionResult<Author>> PostAuthor(Author author)
+        {
+            _context.Authors.Add(author);
+
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetAuthor),
+                new { id = author.Id }, author);
+        }
+
+        // GET: api/authors/1/books
+        [HttpGet("{authorId}/books")]
+        public async Task<ActionResult<IEnumerable<Book>>> GetBooksByAuthor(int authorId)
+        {
+            var author = await _context.Authors
+                .Include(a => a.Books)
+                .FirstOrDefaultAsync(a => a.Id == authorId);
+
+            if (author == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(author.Books);
+        }
+    }
+}
