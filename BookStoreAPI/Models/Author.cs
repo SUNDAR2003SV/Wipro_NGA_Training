0418702697204c1e4b687796cab1c6a55553ace9
SUNDAR2003SV <120670633+SUNDAR2003SV@users.noreply.github.com>
2026-06-01 10:35:54 +0530

    Add files via upload

---
+++ BookStoreAPI/Models/Author.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStoreApi.Models
+{
+    public class Author
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public List<Book>? Books { get; set; }
+    }
+}
--- BookStoreAPI/Models/Book.cs
+++ BookStoreAPI/Models/Book.cs
@@ -1,1 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace BookStoreApi.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public int PublicationYear { get; set; }
+
+        [ForeignKey("Author")]
+        public int AuthorId { get; set; }
+
+        public Author? Author { get; set; }
+    }
+}
