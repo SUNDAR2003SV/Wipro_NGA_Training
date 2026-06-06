83d639ca48ce333dff9818bbf81b4953ea0075b1
SUNDAR2003SV <120670633+SUNDAR2003SV@users.noreply.github.com>
2026-06-06 14:18:57 +0530

    Add files via upload

---
+++ Capstone_Project/HRManagement/HRManagement.Web/Pages/Dashboard/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HRManagement.Web.Pages.Dashboard
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
