1c665188cdd74140da73a66079b5f7c723f230c4
SUNDAR2003SV <120670633+SUNDAR2003SV@users.noreply.github.com>
2026-06-06 14:11:04 +0530

    Add files via upload

---
+++ Capstone_Project/HRManagement/HRManagement.Application/Services/LeaveService.cs
@@ -0,0 +1,49 @@
+﻿using HRManagement.Core.Entities;
+using HRManagement.Core.Exceptions;
+using HRManagement.Core.Interfaces;
+
+namespace HRManagement.Application.Services
+{
+    public class LeaveService
+    {
+        // Repository dependency
+        private readonly ILeaveRepository _leaveRepository;
+
+        // Constructor Injection
+        public LeaveService(ILeaveRepository leaveRepository)
+        {
+            _leaveRepository = leaveRepository;
+        }
+
+        // Apply Leave
+        public async Task ApplyLeaveAsync(Leave leave)
+        {
+            // Business Rule:
+            // Start date cannot be after end date
+            if (leave.StartDate > leave.EndDate)
+            {
+                throw new InvalidLeaveException(
+                    "Start date cannot be greater than end date.");
+            }
+
+            // Business Rule:
+            // Leave cannot be applied for past dates
+            if (leave.StartDate.Date < DateTime.Today)
+            {
+                throw new InvalidLeaveException(
+                    "Leave cannot be applied for past dates.");
+            }
+
+            // Default Status
+            leave.Status = "Pending";
+
+            await _leaveRepository.AddAsync(leave);
+        }
+
+        // Get all leave requests
+        public async Task<IEnumerable<Leave>> GetAllLeavesAsync()
+        {
+            return await _leaveRepository.GetAllAsync();
+        }
+    }
+}
