b6d96640bccebe94941f54941bc3a8669476b0db
SUNDAR2003SV <120670633+SUNDAR2003SV@users.noreply.github.com>
2026-06-06 14:09:12 +0530

    Add files via upload

---
+++ Capstone_Project/HRManagement/HRManagement.Core/Exceptions/EmployeeNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿namespace HRManagement.Core.Exceptions
+{
+    // Custom exception for employee not found
+    public class EmployeeNotFoundException : Exception
+    {
+        public EmployeeNotFoundException(int id)
+            : base($"Employee with Id {id} not found.")
+        {
+        }
+    }
+}
--- Capstone_Project/HRManagement/HRManagement.Core/Exceptions/InvalidLeaveException.cs
+++ Capstone_Project/HRManagement/HRManagement.Core/Exceptions/InvalidLeaveException.cs
@@ -1,1 +1,10 @@
-
+﻿namespace HRManagement.Core.Exceptions
+{
+    public class InvalidLeaveException : Exception
+    {
+        public InvalidLeaveException(string message)
+            : base(message)
+        {
+        }
+    }
+}
