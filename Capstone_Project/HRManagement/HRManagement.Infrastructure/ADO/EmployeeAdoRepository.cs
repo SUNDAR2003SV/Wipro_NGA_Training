feec481a40130fea2d27400b77d77a2bd6871e9f
SUNDAR2003SV <120670633+SUNDAR2003SV@users.noreply.github.com>
2026-06-06 14:12:51 +0530

    Add files via upload

--- Capstone_Project/HRManagement/HRManagement.Infrastructure/ADO/EmployeeAdoRepository.cs
+++ Capstone_Project/HRManagement/HRManagement.Infrastructure/ADO/EmployeeAdoRepository.cs
@@ -1,1 +1,34 @@
+﻿using Microsoft.Data.SqlClient;
 
+namespace HRManagement.Infrastructure.ADO
+{
+    public class EmployeeAdoRepository
+    {
+        private readonly string _connectionString;
+
+        public EmployeeAdoRepository(
+            string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        // Get Total Employee Count
+        public async Task<int> GetEmployeeCountAsync()
+        {
+            using SqlConnection connection =
+                new SqlConnection(_connectionString);
+
+            await connection.OpenAsync();
+
+            SqlCommand command =
+                new SqlCommand(
+                    "SELECT COUNT(*) FROM Employees",
+                    connection);
+
+            var result =
+                await command.ExecuteScalarAsync();
+
+            return Convert.ToInt32(result);
+        }
+    }
+}
