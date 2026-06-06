e0e19cd0ad67936983152f43b080c9d6e02d2275
SUNDAR2003SV <120670633+SUNDAR2003SV@users.noreply.github.com>
2026-06-06 14:13:14 +0530

    Add files via upload

--- Capstone_Project/HRManagement/HRManagement.Infrastructure/Data/HRDbContext.cs
+++ Capstone_Project/HRManagement/HRManagement.Infrastructure/Data/HRDbContext.cs
@@ -1,1 +1,33 @@
+﻿using HRManagement.Core.Entities;
+using Microsoft.EntityFrameworkCore;
 
+namespace HRManagement.Infrastructure.Data
+{
+    public class HRDbContext : DbContext
+    {
+        public HRDbContext(
+            DbContextOptions<HRDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+
+        public DbSet<Leave> Leaves { get; set; }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<AuditLog> AuditLogs { get; set; }
+
+        protected override void OnModelCreating(
+            ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Leave>()
+                .HasOne(l => l.Employee)
+                .WithMany(e => e.Leaves)
+                .HasForeignKey(l => l.EmployeeId);
+        }
+    }
+}
