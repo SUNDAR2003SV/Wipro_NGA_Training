f4ccaadb3c75b6ee8829cf61410da5b8785a1c82
SUNDAR2003SV <120670633+SUNDAR2003SV@users.noreply.github.com>
2026-06-12 21:50:58 +0530

    Add files via upload

---
+++ Capstone_Project/HRManagement/HRManagement.Web/Views/AuditLog/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HRManagement.Web.Views.Audit
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
