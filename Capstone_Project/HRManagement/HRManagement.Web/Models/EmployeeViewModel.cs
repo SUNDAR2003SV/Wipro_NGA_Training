7cb3ea25c14a464a052a58ee4b348928a63c2ba0
SUNDAR2003SV <120670633+SUNDAR2003SV@users.noreply.github.com>
2026-06-12 21:49:29 +0530

    Add files via upload

---
+++ Capstone_Project/HRManagement/HRManagement.Web/Models/EmployeeViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HRManagement.Web.Models
+{
+    public class EmployeeViewModel
+    {
+        public int EmployeeId { get; set; }
+
+        [Required(ErrorMessage = "First Name is required")]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Last Name is required")]
+        public string LastName { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        public string Department { get; set; } = string.Empty;
+    }
+}
--- Capstone_Project/HRManagement/HRManagement.Web/Models/LoginViewModel.cs
+++ Capstone_Project/HRManagement/HRManagement.Web/Models/LoginViewModel.cs
@@ -1,1 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
 
+namespace HRManagement.Web.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        public string Username { get; set; }
+            = string.Empty;
+
+        [Required]
+        public string Password { get; set; }
+            = string.Empty;
+    }
+}
